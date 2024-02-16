8a8980d4119ea95dc488e71e2f4f6576c3471891
Florian Vallen <florian.vallen@gmail.com>
2024-02-16 16:45:34 +0100

    📸: using webcam to take photo

--- src/components/webcam/CaptureAndUpload.tsx
+++ src/components/webcam/CaptureAndUpload.tsx
@@ -1,9 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
 
 const CaptureAndUpload: React.FC = () => {
-  return <div></div>;
+  const webcamRef = useRef<Webcam>(null);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setImageSrc(imageSrc);
+    }
+  };
+
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={640}
+        height={480}
+      />
+      <button onClick={capture}>Capture</button>
+      {imageSrc && (
+        <div>
+          <h2>Preview:</h2>
+          <img src={imageSrc} alt="Captured" />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CaptureAndUpload;
