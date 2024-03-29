0f2ea4332329e0cb81c04bdc5303473e3f8a5951
Florian Vallen <florian.vallen@gmail.com>
2024-03-29 15:37:24 +0100

    feat: you have to be logged in on /alienware as well

--- src/app/alienware/page.tsx
+++ src/app/alienware/page.tsx
@@ -2,8 +2,17 @@ export const revalidate = 3;
 
 import { Suspense } from "react";
 import { Wizard } from "@/components/wizard/Wizard";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 
 export default async function Alienware() {
+  const session = await auth();
+
+  // redirect to '/' if not authenticated
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <Suspense>
       <Wizard initialStep="SelectCameraOrFile" />
