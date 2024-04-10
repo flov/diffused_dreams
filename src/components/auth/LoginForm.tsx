2fd66683d2f19ea4cb740f8fce969226dbb5abde
xdobx <f77cb@posteo.de>
2024-04-10 13:00:00 +0200

    route after login to mainpage

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -29,7 +29,7 @@ export default function LoginForm() {
       email: data.email,
       password: data.password,
       redirect: true,
-      callbackUrl: "/eventstation",
+      callbackUrl: "/",
     });
 
     setLoading(false);
